a64f9476aacec5860e822ace411fdba45782a785
SirkoKropyvnytskyi <gorbatuk46566@gmail.com]>
2025-06-10 21:28:42 +0300

    Changed generator initialization

--- examples/generatorDemo.js
+++ examples/generatorDemo.js
@@ -1,6 +1,6 @@
 import { colorCycle } from '../lib/generator.js';
 
-const gen = colorCycle();
+const gen = colorCycle(1);
 
 for (let i = 0; i < 6; i++) {
   console.log(gen.next().value); // red, green, blue, red, green, blue
